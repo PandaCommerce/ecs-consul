182565989a068a3212de7a5ac9312eb6d8919bb3
root <root@ip-10-0-0-140.eu-west-1.compute.internal>
2016-02-27 09:36:36 +0000

    added aws cli

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
 FROM progrium/consul
 ADD consul_on_ec2_hostname.sh /usr/local/bin/consul_on_ec2_hostname.sh
-ADD 
+ADD awscli-bundle /tmp/awscli-bundle
+
+RUN apk add --update \
+    python \
+    python-dev \
+    py-pip \
+    build-base \
+  && pip install virtualenv \
+  && rm -rf /var/cache/apk/*
+RUN /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 ENTRYPOINT ["consul_on_ec2_hostname.sh"]
--- consul_on_ec2_hostname.sh
+++ consul_on_ec2_hostname.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 export LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-echo Running: unzip awscli-bundle.zip
-exec unzip awscli-bundle.zip
-echo Running: sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-exec sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+echo Running: /bin/consul agent -server -advertise $LOCAL_IP "$@" -bootstrap -retry-join 192.168.0.1 -data-dir="/tmp/consul"
 exec /bin/consul agent -server -advertise $LOCAL_IP "$@" -bootstrap -retry-join 192.168.0.1 -data-dir="/tmp/consul"
